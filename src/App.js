977d915e64182cb9e030ece0ab1cccb131156e62
Jola Klodowska <jola.klodowska@wp.pl>
2022-03-23 11:36:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TimeCounter from "./components/TimeCounter/TimeCounter";
+import Button from "./components/Button/Button";
+
+const App = () => {
+  return (
+    <div>
+      <TimeCounter />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TimeCounter/TimeCounter.js
@@ -0,0 +1,7 @@
+const TimeCounter = () => {
+    return (
+        <h2>TimeCounter</h2>
+    );
+}
+
+export default TimeCounter;
